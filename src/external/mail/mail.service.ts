ab0e7129b3460a794c73b30c755f0ff8e7f114fc
Anh Nguyễn Hoàng <anh.nh1@teko.vn>
2022-04-10 17:02:32 +0700

    chore: update mail template

--- src/external/mail/mail.service.ts
+++ src/external/mail/mail.service.ts
@@ -9,7 +9,7 @@ export class MailService {
     await this.mailerService.sendMail({
       to: email,
       subject: 'Confirm your Email',
-      html: `<div style="width:640px;margin:auto;padding:10px 20px 25px;font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"><h2 style="margin:0;paddimng-bottom:8px;border-bottom:1px solid #d0d0d0;font-size:30px;">AH Ticker</h2><p style="margin:16px 0 20px;">Thank you for creating an account in our app. To complete the registration process, please enter the verification code below:</p><p style="font-size:18px;font-family:Roboto;font-weight:700">${otp}</p><p style=" margin:6px 0 0;font-size:13px;">The AH Ticker Team.</p></div>`,
+      html: `<div style="width:640px;margin:auto;padding:10px 20px 25px;font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"><h2 style="margin:0;paddimng-bottom:8px;border-bottom:1px solid #d0d0d0;font-size:30px;">AH Ticker</h2><p style="margin:16px 0 20px;">Thank you for creating an account in our app. To complete the registration process, please enter the verification code below:</p><p style="font-size:18px;font-family:Roboto;font-weight:700">${otp}</p><p style=" margin:6px 0 0;font-size:13px;">The AH Ticker Team</p></div>`,
     });
   }
 
@@ -17,7 +17,7 @@ export class MailService {
     await this.mailerService.sendMail({
       to: email,
       subject: 'Reset password',
-      html: `<div style="width:640px;margin:auto;padding:10px 20px 25px;font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"><h2 style="margin:0;paddimng-bottom:8px;border-bottom:1px solid #d0d0d0;font-size:30px;">AH Ticker</h2><p style="margin:16px 0 20px;">We got a request to reset your password. To complete the registration process, please enter the verification code below:</p><p style="font-size:18px;font-family:Roboto;font-weight:700">${otp}</p<p style=" margin:6px 0 0;font-size:13px;">The AH Ticker Team.</p></div>`,
+      html: `<div style="width:640px;margin:auto;padding:10px 20px 25px;font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"><h2 style="margin:0;paddimng-bottom:8px;border-bottom:1px solid #d0d0d0;font-size:30px;">AH Ticker</h2><p style="margin:16px 0 20px;">We got a request to reset your password. To complete the registration process, please enter the verification code below:</p><p style="font-size:18px;font-family:Roboto;font-weight:700">${otp}</p<p style=" margin:6px 0 0;font-size:13px;">The AH Ticker Team</p></div>`,
     });
   }
 }
